49541b622c5b6f5f3cc06c4006a0df1c4cd3c9d0
Nahuel Ambrosini <nambrosini@onapsis.com>
2019-06-02 21:20:41 -0300

    Add init_db shortcut

--- slacker/database.py
+++ slacker/database.py
@@ -1,3 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+def init_db():
+    import slacker.models
+    from slacker import create_app
+    app = create_app()
+    with app.app_context():
+        db.create_all()
---
+++ slacker/manage.py
@@ -0,0 +1,39 @@
+import click
+import os
+
+from flask.cli import with_appcontext
+
+from slacker.database import init_db
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.join(HERE, os.pardir)
+TEST_PATH = os.path.join(PROJECT_ROOT, 'tests')
+
+
+@click.command()
+def test():
+    """Run the tests."""
+    import pytest
+    ret = pytest.main([TEST_PATH, '--verbose'])
+    exit(ret)
+
+
+@click.command()
+def clean():
+    """Remove *.pyc and *.pyo files recursively starting at current directory.
+    Borrowed from Flask-Script, converted to use Click.
+    """
+    for dirpath, dirnames, filenames in os.walk('.'):
+        for filename in filenames:
+            if filename.endswith('.pyc') or filename.endswith('.pyo'):
+                full_pathname = os.path.join(dirpath, filename)
+                click.echo('Removing {}'.format(full_pathname))
+                os.remove(full_pathname)
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
