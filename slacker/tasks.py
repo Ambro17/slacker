4781e159e1bb60946887a0b17237702c672af0c3
Ambro17 <Ambro17.1@gmail.com>
2019-07-08 12:23:16 -0300

    Add error msg to async task

--- slacker/tasks.py
+++ slacker/tasks.py
@@ -6,5 +6,5 @@ from slacker.celery import celery
 @celery.task
 def send_message_async(message):
     r = current_app.slack_cli.api_call('chat.postMessage', text=message, channel='#general')
-    return r['ok']
+    return r['ok'], r.get('error')
 
